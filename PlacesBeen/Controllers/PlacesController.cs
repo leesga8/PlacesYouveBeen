c6ef42b5eb1213519ae6bd92b31d0a945f826d12
Albert Lee <leesga8@gmail.com>
2021-12-16 18:53:27 -0800

    display all places in list. add delete button

--- PlacesBeen/Controllers/PlacesController.cs
+++ PlacesBeen/Controllers/PlacesController.cs
@@ -6,5 +6,33 @@ namespace PlacesBeen.Controllers
 {
   public class PlacesController : Controller
   {
+
+    [HttpGet("/places")]
+    public ActionResult Index()
+    {
+      List<Place> allPlaces = Place.GetAll();
+      return View(allPlaces);
+    }
+
+    [HttpGet("/places/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/places")]
+    public ActionResult Create(string cityname)
+    {
+      Place myPlace = new Place(cityname);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/places/delete")]
+    public ActionResult DeleteAll()
+    {
+      Place.ClearAll();
+      return View();
+
+    }
   }
 }
